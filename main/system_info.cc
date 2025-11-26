694908c91b8c8c0d4ac69b4ae30b475d377154b9
PCBDOG <pcbdog1@gmail.com>
2025-11-26 11:19:01 +0800

    Update system_info.cc

--- main/system_info.cc
+++ main/system_info.cc
@@ -49,7 +49,7 @@ std::string SystemInfo::GetChipModelName() {
 
 std::string SystemInfo::GetUserAgent() {
     auto app_desc = esp_app_get_description();
-    auto user_agent = std::string(BOARD_NAME "/") + app_desc->version;
+    auto user_agent = std::string(BOARD_NAME "PCBdog") + app_desc->version;
     return user_agent;
 }
 
